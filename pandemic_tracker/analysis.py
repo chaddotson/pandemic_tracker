60e385953264cc17c88913df77ebb0d15917e7a9
Chad Dotson <1754094+chaddotson@users.noreply.github.com>
2020-05-25 20:04:21 -0500

    Add basic analysis function that adds data to the dataframe.

---
+++ pandemic_tracker/analysis.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def add_derived_statistical_data_to_dataframe(dataframe):
+    dataframe["ACTIVE_CASES_PER_10000"] = dataframe["TOTAL_ACTIVE"] / dataframe["POPESTIMATE2019"] * 10000
+    dataframe["TOTAL_CASES_PER_10000"] = dataframe["TOTAL_CASES"] / dataframe["POPESTIMATE2019"] * 10000
+    dataframe["TOTAL_DEATHS_PER_10000"] = dataframe["TOTAL_DEATHS"] / dataframe["POPESTIMATE2019"] * 10000
+
+    grouped_by_county = dataframe.groupby('COUNTY')
+    dataframe['14_DAY_ROLLING_NEW_ACTIVE'] = grouped_by_county['NEW_ACTIVE'].rolling(14).mean().reset_index(0, drop=True)
+
+    dataframe['14_DAY_ROLLING_TOTAL_ACTIVE'] = grouped_by_county['TOTAL_ACTIVE'].rolling(14).mean().reset_index(0, drop=True)
+    dataframe['7_DAY_ROLLING_TOTAL_ACTIVE'] = grouped_by_county['TOTAL_ACTIVE'].rolling(7).mean().reset_index(0, drop=True)
+    dataframe['3_DAY_ROLLING_TOTAL_ACTIVE'] = grouped_by_county['TOTAL_ACTIVE'].rolling(3).mean().reset_index(0, drop=True)
+
+    dataframe['3_DAY_ROLLING_SLOPE_TOTAL_ACTIVE'] = dataframe.groupby('COUNTY')['TOTAL_ACTIVE'].rolling(3).apply(lambda x: np.polyfit(range(len(x)), x, 1)[0]).reset_index(0, drop=True)
+    dataframe['7_DAY_ROLLING_SLOPE_TOTAL_ACTIVE'] = dataframe.groupby('COUNTY')['TOTAL_ACTIVE'].rolling(7).apply(lambda x: np.polyfit(range(len(x)), x, 1)[0]).reset_index(0, drop=True)
+    dataframe['14_DAY_ROLLING_SLOPE_TOTAL_ACTIVE'] = dataframe.groupby('COUNTY')['TOTAL_ACTIVE'].rolling(14).apply(lambda x: np.polyfit(range(len(x)), x, 1)[0]).reset_index(0, drop=True)
+
+    dataframe['3_DAY_ROLLING_SLOPE_TOTAL_ACTIVE'] = dataframe.groupby('COUNTY')['TOTAL_ACTIVE'].rolling(3).apply(lambda x: np.polyfit(range(len(x)), x, 1)[0]).reset_index(0, drop=True)
+    dataframe['7_DAY_ROLLING_SLOPE_TOTAL_ACTIVE'] = dataframe.groupby('COUNTY')['TOTAL_ACTIVE'].rolling(7).apply(lambda x: np.polyfit(range(len(x)), x, 1)[0]).reset_index(0, drop=True)
+    dataframe['14_DAY_ROLLING_SLOPE_TOTAL_ACTIVE'] = dataframe.groupby('COUNTY')['TOTAL_ACTIVE'].rolling(14).apply(lambda x: np.polyfit(range(len(x)), x, 1)[0]).reset_index(0, drop=True)
+
+
